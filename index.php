e5324c14a4ecfa9da92e4f164d4d709fb2289317
Websitehosterio <124309800+Websitehosterio@users.noreply.github.com>
2023-10-14 14:07:51 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<html>
+ <head>
+   <title>WebsiteHosterIO - GitHub</title>
+   <link rel="stylesheet" href="css/styles.css"/>
+ </head>
+ <body>
+
+   <!-- Navigation Bar -->
+   <nav class="navbar">
+
+     <!-- Div for links -->
+     <div class="links">
+       <ul>
+         <li><a href="repositories/">View your repositories</a></li>
+       </ul>
+    </div>
+   </nav>
+ </body>
+ </head>  
+</html>
